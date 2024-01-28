7428d49c981be35d764cd8ae1def1a60b7eb87f8
Michelon Souza <michelon.souza@hotmail.com>
2024-01-28 13:43:42 -0300

    feat(misc): add routes and adjust styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  CreateThoughtComponent,
+  ThoughtListComponent,
+  DeleteThoughtComponent,
+  EditThoughtComponent,
+} from './components/thoughts';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list-thought',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list-thought',
+    component: ThoughtListComponent,
+  },
+  {
+    path: 'create-thought',
+    component: CreateThoughtComponent,
+  },
+  {
+    path: 'delete-thought/:id',
+    component: DeleteThoughtComponent,
+  },
+  {
+    path: 'edit-thought/:id',
+    component: EditThoughtComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent, FooterComponent } from './components';
+import {
+  HeaderComponent,
+  FooterComponent,
+  ThoughtComponent,
+  ThoughtListComponent,
+  CreateThoughtComponent,
+} from './components';
+import { DeleteThoughtComponent } from './components/thoughts/delete-thought/delete-thought.component';
+import { EditThoughtComponent } from './components/thoughts/edit-thought/edit-thought.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtComponent,
+    ThoughtListComponent,
+    ThoughtComponent,
+    DeleteThoughtComponent,
+    EditThoughtComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
